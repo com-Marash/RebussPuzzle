8ac00a00629fef27a1d72908c6997398690f4512
Maedeh <maedeh.hm21@gmail.com>
2017-03-01 11:07:47 -0500

    Back Button was managed.

--- app/src/main/java/marash/com/rebuspuzzle/selected_image/WinTransitionActivity.java
+++ app/src/main/java/marash/com/rebuspuzzle/selected_image/WinTransitionActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import marash.com.rebuspuzzle.R;
+import marash.com.rebuspuzzle.dto.DataBaseCreator;
 import marash.com.rebuspuzzle.main_page.MainActivity;
 
 /**
@@ -26,13 +27,20 @@ public class WinTransitionActivity extends AppCompatActivity {
 
     public void NextLevelFunction(View view) {
         int nextLevelPosition = getIntent().getIntExtra("nextLevelPosition", -1);
-        if(nextLevelPosition <= MainActivity.gameCellArray.size()) {
+        if(nextLevelPosition <= DataBaseCreator.gameCellArray.size()) {
             Intent intent = new Intent(WinTransitionActivity.this, SelectedImageActivity.class);
-            intent.putExtra("gameCellInfo", MainActivity.gameCellArray.get(nextLevelPosition - 1));
+            intent.putExtra("gameCellInfo", DataBaseCreator.gameCellArray.get(nextLevelPosition - 1));
             startActivity(intent);
         }else {
             Intent intent = new Intent(WinTransitionActivity.this, MainActivity.class);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public void onBackPressed(){
+        Intent intent = new Intent(WinTransitionActivity.this, MainActivity.class );
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
