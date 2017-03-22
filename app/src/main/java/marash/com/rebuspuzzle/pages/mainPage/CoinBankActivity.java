3254fa31054eef88dc1223ca4edcc138a83777c6
Maedeh <maedeh.hm21@gmail.com>
2017-03-22 15:20:16 -0400

    coinBank page created and improved.

--- app/src/main/java/marash/com/rebuspuzzle/pages/mainPage/CoinBankActivity.java
+++ app/src/main/java/marash/com/rebuspuzzle/pages/mainPage/CoinBankActivity.java
@@ -1,6 +1,10 @@
 package marash.com.rebuspuzzle.pages.mainPage;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import marash.com.rebuspuzzle.R;
 
 /**
  * Created by Maedeh on 3/21/2017.
@@ -8,6 +12,24 @@ import android.support.v7.app.AppCompatActivity;
 
 public class CoinBankActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.coin_bank);
+    }
 
+    public void buyCoin(View view) {
 
+        switch (view.getId()){
+            case R.id.smallCoinPrice:
+                //TODO buying.
+                break;
+            case R.id.mediumCoinPrice:
+
+                break;
+            case R.id.largeCoinPrice:
+
+                break;
+        }
+    }
 }
